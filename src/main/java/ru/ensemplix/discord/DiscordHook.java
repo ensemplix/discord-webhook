a4186c1b157ceb3682f3b60f12ec3a003d9543be
ensirius <ensirius@gmail.com>
2017-04-01 14:43:00 +0300

    Добавлена возможность прикреплять файлы

--- src/main/java/ru/ensemplix/discord/DiscordHook.java
+++ src/main/java/ru/ensemplix/discord/DiscordHook.java
@@ -3,6 +3,8 @@ package ru.ensemplix.discord;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 
+import java.io.File;
+
 public class DiscordHook {
 
     private static final Gson gson = new Gson();
@@ -16,6 +18,15 @@ public class DiscordHook {
         HttpRequest request = HttpRequest.post(url)
                 .userAgent("Discord Hook")
                 .send(gson.toJson(message));
+
+        return gson.fromJson(request.reader(), DiscordResponse.class);
+    }
+
+    public DiscordResponse sendMessage(DiscordMessage message, File file) {
+        HttpRequest request = HttpRequest.post(url)
+                .userAgent("Discord Hook")
+                .part("payload_json", gson.toJson(message))
+                .part("file", file.getName(), file);
 
         return gson.fromJson(request.reader(), DiscordResponse.class);
     }
